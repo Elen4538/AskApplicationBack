ab4cd43203f7b408ac1f5202f8add2ab7d610034
Elen4538 <mpl4538@gmail.com>
2023-05-22 18:47:50 +0100

    Initial commit

---
+++ src/main/java/com/askapp/helen/spring/askapplicationback/AskApplicationBackApplication.java
@@ -0,0 +1,13 @@
+package com.askapp.helen.spring.askapplicationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskApplicationBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskApplicationBackApplication.class, args);
+	}
+
+}
